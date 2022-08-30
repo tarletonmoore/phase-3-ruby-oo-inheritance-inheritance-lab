b6d47e94752ab48effce597fc683a4f55f6d9765
John Tarleton Moore <tarletonmoore@gmail.com>
2022-08-30 12:34:32 -0400

    commit tree 5632750d7621c9489f224a782e81fddeb7817ed0

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+require_relative "./user"
+class Student < User
+attr_reader :knowledge
+    def initialize(knowledge=[])
+        @knowledge = knowledge
+    end
+
+    def learn(string="")
+@knowledge << string
+    end
+
 
 end
